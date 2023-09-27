cd8d478a2b559d62479599f3a3120f3438e7e0a4
Nishanthika2005 <144510938+Nishanthika2005@users.noreply.github.com>
2023-09-27 17:52:24 +0530

    Add files via upload

---
+++ main (3).py
@@ -0,0 +1,14 @@
+# initialize a list
+my_list = [1, 2, 3, 1, 5, 4]
+ 
+# find length of the list
+list_size = len(my_list)
+ 
+# declare for loop
+for itr in range(list_size):
+ 
+      # check the condition
+    if(my_list[itr] == 1):
+ 
+          # print the indices
+        print(itr)
